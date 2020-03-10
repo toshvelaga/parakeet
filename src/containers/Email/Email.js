98fda1ea4e614a2d7d2dfc4b5242176473c3ee06
SanthoshVelaga <toshvelaga@gmail.com>
2020-03-10 14:08:19 -0700

    added state and helper functions to email.js

--- src/containers/Email/Email.js
+++ src/containers/Email/Email.js
@@ -2,15 +2,29 @@ import React, { Component } from 'react';
 import ParakeetHeader from '../../../src/components/ParakeetHeader/ParakeetHeader'
 import './Email.css'
 
+import { withRouter } from 'react-router-dom';
+
+// todo: need to add email validation
+
 class Email extends Component {
-    state = {  }
+    state = { value: '' }
+
+    InputHandler = (e) => {
+        this.setState({value: e.target.value})
+    }
+
+    onSubmit = () => {
+        this.props.history.push("/Rating");
+    }
+
     render() { 
+        {console.log(this.state.value)}
         return (<div>
             <ParakeetHeader />
-            <input className="emailInput" type="email" id="email" name="email" placeholder="Enter your email"></input>
-            <button className="emailButton">Submit</button>
+            <input value={this.state.value} onChange={this.InputHandler} className="emailInput" type="email" id="email" name="email" placeholder="Enter your email"></input>
+            <button onClick={this.onSubmit} className="emailButton">Submit</button>
         </div>);
     }
 }
  
-export default Email;
+export default withRouter(Email);
