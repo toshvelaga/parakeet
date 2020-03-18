34a2df7b932d255cadf48b687448c7f41e33ae44
SanthoshVelaga <toshvelaga@gmail.com>
2020-03-18 01:04:02 -0700

    finished setting up textarea value change and redux

--- src/consumer/containers/Textarea/Textarea.js
+++ src/consumer/containers/Textarea/Textarea.js
@@ -9,22 +9,17 @@ import { connect } from 'react-redux';
 // Need to change color of the textinput on click
 // SO answer: https://stackoverflow.com/questions/56803234/reactjs-material-ui-textfield-change-color-label-and-underline-activity-field-in
 
+// Adding redux to a textarea/input tutorial: https://www.youtube.com/watch?v=MOO5hit7s0U
+
 class Textarea extends Component {
     constructor(props) {
     super(props)
         this.state = { 
             show: 'false',
-            // value: '',
         }
     }
 
-    // onChangeHander = (e) => {
-    //     // this.setState({value: e.target.value})
-    //     store.dispatch({type: 'TEXTAREA_VALUE'})
-    // }
-
     render() { 
-    // console.log(this.state.value)
     
     const style = {
         width: '85%',
@@ -56,7 +51,9 @@ const mapStatetoProps = (state) => {
 const mapDispatchtoProps = (dispatch) => {
     return {
         inputChanged: (e) => {
-            console.log('changed', e.target.value)
+            // console.log('changed', e.target.value);
+            const action = { type: 'TEXTAREA_CHANGE', text: e.target.value }
+            dispatch(action)
         }
     }
 }
--- src/store/reducers/textareaReducer.js
+++ src/store/reducers/textareaReducer.js
@@ -1,13 +1,12 @@
-import { TEXTAREA_VALUE } from '../constants/action-types'
+// import { TEXTAREA_VALUE } from '../constants/action-types'
 
 const initialState = {
     textValue: '',
   };
   
   const textareaReducer = (state = initialState, action) => {
-      console.log('reducer', action)
       switch (action.type) {
-        case 'INPUT_CHANGE':
+        case 'TEXTAREA_CHANGE':
           return {...initialState, textValue: action.text };
         default:
           return state;
