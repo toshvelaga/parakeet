a20b30dfb58a58284d9ef7c05fcd61486c29f655
SanthoshVelaga <toshvelaga@gmail.com>
2020-03-21 21:36:04 -0700

    turned reviews into a functional component and added props

---
+++ src/businesses/components/Reviews/Reviews.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './Reviews.css';
+import StarRateIcon from '@material-ui/icons/StarRate';
+
+// https://codepen.io/toshvelaga/pen/vYYLdbV
+
+const Reviews = (props) => {
+    
+   function Star(n) {
+       if (n < 4) {
+           return <StarRateIcon style={{color: '#f40054', fontSize: '2rem'}}/>
+       } else 
+       return <StarRateIcon style={{color: '#0378d8', fontSize: '2rem'}}/>
+   }
+
+    return (
+        <div className="review">
+            <div className="top_row">
+                <span style={{float: 'left'}} className="rating">
+                    {[...Array(props.n)].map((e, i) => <span key={i}>
+                        {Star(props.n)}
+                    </span>)}
+                </span>
+                <span style={{float: 'right'}} className="date">{props.date}</span>
+            </div>
+            <p style={{marginTop:'3rem', textAlign: 'left'}}>{props.text} {"––"} {props.email} </p>
+        </div>
+        );
+    }
+ 
+export default Reviews;
+
+
+
+
--- src/businesses/containers/Feed/Feed.js
+++ src/businesses/containers/Feed/Feed.js
@@ -2,16 +2,32 @@ import React, { Component } from 'react';
 import Navbar from '../../components/Navbar/Navbar'
 import './Feed.css'
 
-import Reviews from '../Reviews/Reviews'
+import Reviews from '../../components/Reviews/Reviews'
 
 class Feed extends Component {
     state = {  }
+
+    reviewData = {
+        data1 : {
+            text: "Great service!",
+            rating: 4,
+            date: '3/20/20',
+            email: 'toshvelaga@gmail.com'
+        },
+        data2 : {
+            text: "The service could use improvement. I was very dissapointed.",
+            rating: 2,
+            date: '3/20/20',
+            email: 'igor@gmail.com'
+        }
+   }
+
     render() { 
         return (
         <div>
             <Navbar />
             <h2 style={{marginTop: 0}}>Feed</h2>
-            <Reviews />
+            <Reviews text="Great service!" n={this.reviewData.data1.rating} email="toshvelaga@gmail.com" date="3/20/20"  />
         </div>);
     }
 }
