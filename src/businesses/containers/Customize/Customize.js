e06b62b9a26d8a87ed3179aa1555b0348ca38dd6
SanthoshVelaga <toshvelaga@gmail.com>
2020-03-22 21:54:31 -0700

    added styling to cutomize container

--- src/businesses/containers/Customize/Customize.js
+++ src/businesses/containers/Customize/Customize.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 import Navbar from '../../components/Navbar/Navbar'
 import QRcode from '../../components/QRcode/QRcode'
+import Button from '@material-ui/core/Button'
+import TextField from '@material-ui/core/TextField';
+import SaveIcon from '@material-ui/icons/Save';
+
+import './Customize.css'
 
 class Customize extends Component {
     state = {  }
@@ -9,15 +14,72 @@ class Customize extends Component {
         <div>
             <Navbar />
             <h2>Customize</h2>
-            <span>Enter in business email: <input type="text"></input></span>
-            <br></br><br></br>
-            <span>Enter in manager email: <input type="text"></input></span>
-            <br></br><br></br>
-            <span>Enter in name of restaurant: <input type="text"></input></span>
+
+            <div className="outerDiv">
+
+            <TextField
+                // id="outlined-full-width"
+                // label="Enter in business email"
+                style={{ margin: '1rem', width: '80%' }}
+                placeholder="Enter name of restaurant"
+                // margin="dense"
+                size="small"
+                // helperText="Enter name of restaurant"
+                fullWidth
+                margin="normal"
+                InputLabelProps={{
+                    shrink: true,
+                }}
+                variant="outlined"
+            />
+
+            <TextField
+                // id="outlined-full-width"
+                // label="Enter in business email"
+                style={{ margin: '1rem', width: '80%' }}
+                placeholder="Enter business email"
+                // margin="dense"
+                size="small"
+                // helperText="Enter in business email"
+                fullWidth
+                margin="normal"
+                InputLabelProps={{
+                    shrink: true,
+                }}
+                variant="outlined"
+            />
+
+            <TextField
+                // id="outlined-full-width"
+                // label="Enter in business email"
+                style={{ margin: '1rem', width: '80%' }}
+                placeholder="Enter manager email"
+                // margin="dense"
+                size="small"
+                // helperText="Enter in manager email"
+                fullWidth
+                margin="normal"
+                InputLabelProps={{
+                    shrink: true,
+                }}
+                variant="outlined"
+            />
+
             <br></br><br></br>
             <QRcode />
             <br></br><br></br>
-            <button style={{backgroundColor: 'lightgreen'}} type="submit">Export QR code</button>
+
+            <Button variant="outlined" color="primary">Export QR Code</Button>
+
+            </div>        
+
+            <div>            
+                {/* <Button style={{color: 'green', border: '1px solid green', width: '15%', marginTop: '2rem'}} variant="outlined" color="primary">Save</Button> */}
+                <Button style={{width: '20%', marginTop: '2rem', marginBottom: '2rem', color: 'white', backgroundColor: '#0378d8'}} variant="contained" color="primary" size="large" startIcon={<SaveIcon />}>
+                    Save
+                </Button>
+            </div>
+
         </div>);
     }
 }
