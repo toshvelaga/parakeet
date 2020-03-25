677ab7d4a975062708620b289373aaf8943d7e8c
SanthoshVelaga <toshvelaga@gmail.com>
2020-03-24 19:08:31 -0700

    added react-redux-firebase and redux-firestore packages

--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './store/reducers/rootReducer';
 import thunk from 'redux-thunk'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
     <Provider store={store}>
