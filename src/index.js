cef59b20cd95b7ec988f1e530da261a361d8a10a
SanthoshVelaga <toshvelaga@gmail.com>
2020-03-25 14:12:20 -0700

    SignIn js component connected to Firestore

--- src/index.js
+++ src/index.js
@@ -5,19 +5,38 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './store/reducers/rootReducer';
 import thunk from 'redux-thunk'
-import { getFirestore } from 'redux-firestore'
-import { getFirebase } from 'react-redux-firebase'
+import { reduxFirestore ,getFirestore, createFirestoreInstance } from 'redux-firestore'
+import { ReactReduxFirebaseProvider ,getFirebase } from 'react-redux-firebase'
+import fbConfig from './firebase/fbConfig'
+import firebase from "firebase/app";
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
-
-ReactDOM.render(
+const store = createStore(
+    rootReducer,
+    compose(
+      applyMiddleware(thunk.withExtraArgument({ getFirestore, getFirebase })),
+      reduxFirestore(fbConfig)
+    )
+  );
+  
+  const rrfProps = {
+    firebase,
+    config: fbConfig,
+    dispatch: store.dispatch,
+    createFirestoreInstance
+  };
+  
+  
+  ReactDOM.render(
     <Provider store={store}>
-    <App />
+      <ReactReduxFirebaseProvider {...rrfProps}>
+        <App />
+      </ReactReduxFirebaseProvider>
     </Provider>,
-    document.getElementById('root'));
+    document.getElementById("root")
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
