960a207782954ffe042a2345d94db96ebbb32e36
SanthoshVelaga <toshvelaga@gmail.com>
2020-03-26 18:23:33 -0700

    styled signup page

--- src/businesses/containers/SignUp/SignUp.js
+++ src/businesses/containers/SignUp/SignUp.js
@@ -4,6 +4,10 @@ import { connect } from 'react-redux'
 import { Redirect } from 'react-router'
 import { signUp } from '../../../store/actions/actions'
 import Header from '../../components/Header/Header'
+import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { Link } from "react-router-dom";
 
 class SignUp extends Component {
   state = {
@@ -30,9 +34,10 @@ class SignUp extends Component {
     return (<>
       <Header />
       <div className="container">
-        <form onSubmit={this.handleSubmit} className="white">
+        <form onSubmit={this.handleSubmit} style={{width: '60%', display: 'inline-block'}}>
           <h5>Sign Up</h5>
-          <div className="input-field">
+
+          {/* <div className="input-field">
             <label>Email</label>
             <input type="email" id="email" onChange={this.handleChange}/>
           </div>
@@ -50,7 +55,79 @@ class SignUp extends Component {
           </div>
           <div className="input-field">
             <button>Sign Up</button>
-          </div>
+          </div> */}
+
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                onChange={this.handleChange}
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="First Name"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                onChange={this.handleChange}
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Last Name"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                onChange={this.handleChange}
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                onChange={this.handleChange}
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+
+          </Grid>
+          <br></br>
+          <Button
+            style={{backgroundColor: '#0378d8'}}
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+          >
+            Sign Up
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Link to="/signin">
+                Already have an account? Sign in
+              </Link>
+            </Grid>
+          </Grid>
+
           <div className="error">
             {authError ? <p>{authError}</p> : null}
           </div>
