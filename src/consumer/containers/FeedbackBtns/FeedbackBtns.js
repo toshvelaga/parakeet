39dbb2ad74bf023d3d393618a8450176e7ba21a5
SanthoshVelaga <toshvelaga@gmail.com>
2020-03-27 18:41:16 -0700

    cleaned up FeedbackBtn.js code

--- src/consumer/containers/FeedbackBtns/FeedbackBtns.js
+++ src/consumer/containers/FeedbackBtns/FeedbackBtns.js
@@ -26,51 +26,39 @@ class FeedbackBtns extends Component {
         this.setState({ active: !currentState });
     };
 
-    nameToState1 = () => {
-        if (this.state.active == true) {
-            console.log("add" + this.props.name)
+    dispatchToStore = () => {
+        const review = store.getState().btnReducer.reviewType
+
+        if (this.state.active == true && review == 'great') {
             const action1 = {type: ADD_GREAT_FEEDBACK, addedName: this.props.name };
             store.dispatch(action1)
-        } else if (this.state.active == false) {
-            console.log("remove" + this.props.name)
+        } else if (this.state.active == false && review == 'great') {
             const action2 = {type: REMOVE_GREAT_FEEDBACK, removedName: this.props.name };
             store.dispatch(action2)
+        } else if (this.state.active == true && review == 'bad') {
+            const action3 = {type: ADD_BAD_FEEDBACK, addedName: this.props.name };
+            store.dispatch(action3)
+        } else if (this.state.active == false && review == 'bad') {
+            const action4 = {type: REMOVE_BAD_FEEDBACK, removedName: this.props.name };
+            store.dispatch(action4)
         }
     }
 
-    nameToState2 = () => {
-        if (this.state.active == true) {
-            console.log("add" + this.props.name)
-            const action1 = {type: ADD_BAD_FEEDBACK, addedName: this.props.name };
-            store.dispatch(action1)
-        } else if (this.state.active == false) {
-            console.log("remove" + this.props.name)
-            const action2 = {type: REMOVE_BAD_FEEDBACK, removedName: this.props.name };
-            store.dispatch(action2)
-        }
-    }
-
-    onClickFx1 = () => {
+    onClickFx = () => {
         this.toggleClass();
-        this.nameToState1();
-    }
-
-    onClickFx2 = () => {
-        this.toggleClass();
-        this.nameToState2();
+        this.dispatchToStore();
     }
 
     render() { 
-        console.log(store.getState().greatFeedReducer)
         
         const reviewType = store.getState().btnReducer.reviewType;
         let button;
 
         if (reviewType == 'great') {
-            button = <button onClick={this.onClickFx1} className={this.state.active ? 'feedbackBtn' : 'greatSelected'}>{this.props.name}</button>
+            button = <button onClick={this.onClickFx} className={this.state.active ? 'feedbackBtn' : 'greatSelected'}>{this.props.name}</button>
         } 
         if (reviewType == 'bad') {
-            button = <button onClick={this.onClickFx2} className={this.state.active ? 'feedbackBtn' : 'badSelected'}>{this.props.name}</button>
+            button = <button onClick={this.onClickFx} className={this.state.active ? 'feedbackBtn' : 'badSelected'}>{this.props.name}</button>
         }
 
         return ( 
