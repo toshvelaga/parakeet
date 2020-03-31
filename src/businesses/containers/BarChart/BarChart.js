755b6f1ffdc79d26b8d48f08337dbd8124a987cb
SanthoshVelaga <toshvelaga@gmail.com>
2020-03-31 00:41:58 -0700

    editing the ratings chart

--- src/businesses/containers/BarChart/BarChart.js
+++ src/businesses/containers/BarChart/BarChart.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import {Bar} from 'react-chartjs-2';
 
+// docs: https://www.chartjs.org/docs/latest/charts/bar.html
+// https://www.educative.io/edpresso/how-to-use-chartjs-to-create-charts-in-react
+
 const Rating = {
   labels: ['5 Stars', '4 Stars', '3 Stars',
            '2 Stars', '1 Star'],
   datasets: [
     {
-      label: 'Rainfall',
+      label: 'Number of Reviews',
       backgroundColor: 'rgba(75,192,192,1)',
       borderColor: 'rgba(0,0,0,1)',
-      borderWidth: 2,
-      data: [65, 59, 80, 81, 56]
+      borderWidth: 1,
+      data: [4, 5, 1, 2, 1]
     }
   ]
 }
@@ -21,15 +24,24 @@ export default class BarChart extends React.Component {
       <div>
         <Bar
           data={Rating}
+          width={'500rem'}
+          height={'300rem'}
           options={{
             title:{
               display:true,
               text:'Average Rating',
-              fontSize:20
+              fontSize:20,
             },
             legend:{
               display:true,
-              position:'right'
+            },
+            scales: {
+              yAxes: [{
+                ticks: {
+                  stepSize: 1,
+                  beginAtZero: true,
+                },
+              }],
             }
           }}
         />
