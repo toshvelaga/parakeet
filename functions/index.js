b8716f0f89ce37fe67900cee9d3cf159061cbaa6
Kyle Shin <kdshin2010@gmail.com>
2020-03-31 17:58:16 -0700

    cloud-functions

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,8 @@
 const functions = require('firebase-functions');
-const cors = require('cors')({ origin: true });
-const admin = require('firebase-admin');
-admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-
-exports.helloWorld = functions.https.onRequest((request, response) => {
- cors(request, response, () => {
-	 response.send("Hello from Firebase!");
- })
-});
-
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
