73f41af6d183368fe11811c35098145d3190fc0e
SanthoshVelaga <toshvelaga@gmail.com>
2020-03-31 19:00:34 -0700

    fixed bug in negative feedback and adding business logic for the bar chart

--- src/businesses/containers/BarChart/BarChart.js
+++ src/businesses/containers/BarChart/BarChart.js
@@ -1,31 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Bar} from 'react-chartjs-2';
+import firebase from '../../../firebase/fbConfig'
+import { connect } from 'react-redux'
 
 // docs: https://www.chartjs.org/docs/latest/charts/bar.html
 // https://www.educative.io/edpresso/how-to-use-chartjs-to-create-charts-in-react
 // color-hex gradient examples: https://www.color-hex.com/color-palette/4699
-const Rating = {
-  labels: ['5 Stars', '4 Stars', '3 Stars',
-           '2 Stars', '1 Star'],
-  datasets: [
-    {
-      label: 'Number of Reviews',
-      backgroundColor: [
-        'rgba(255,193,0,.6)',
-        'rgba(255,154,0,.45)',
-        'rgba(255,116,0,.3)',
-        'rgba(255,77,0,.25)',
-        'rgba(255,0,0,.2)',
-      ],
-      borderColor: 'rgba(0,0,0,.5)',
-      borderWidth: 1,
-      data: [4, 10, 1, 2, 1]
-    }
-  ]
-}
 
-export default class BarChart extends React.Component {
+let db = firebase.firestore();
+
+class BarChart extends Component {
+  state = {
+    reviews: []
+  }
+
+  componentDidMount() {
+      const docRef = db.collection("users").doc(this.props.auth.uid).collection("customers").get()
+      .then(querySnapshot => {
+          querySnapshot.docs.map(doc => {
+              var joined = this.state.reviews.concat(doc.data().rating)
+              this.setState({reviews: joined})
+          });
+      });
+  }
+
   render() {
+    console.log(this.state.reviews)
+
+    const Rating = {
+      labels: ['5 Stars', '4 Stars', '3 Stars',
+               '2 Stars', '1 Star'],
+      datasets: [
+        {
+          label: 'Number of Reviews',
+          backgroundColor: [
+            'rgba(255,193,0,.6)',
+            'rgba(255,154,0,.45)',
+            'rgba(255,116,0,.3)',
+            'rgba(255,77,0,.25)',
+            'rgba(255,0,0,.2)',
+          ],
+          borderColor: 'rgba(0,0,0,.5)',
+          borderWidth: 1,
+          data: [4, 10, 1, 2, 1]
+        }
+      ]
+    }
+
     return (
       <div>
         <Bar
@@ -55,3 +76,11 @@ export default class BarChart extends React.Component {
     );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+      auth: state.firebase.auth
+  }
+}
+
+export default connect(mapStateToProps, null)(BarChart);
