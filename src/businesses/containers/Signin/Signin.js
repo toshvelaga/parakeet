a9f1b84545430ee4750340380e256fd9cf9c8f19
Tosh Velaga <toshvelaga@gmail.com>
2021-09-11 17:35:48 -0500

    update signin

--- src/businesses/containers/Signin/Signin.js
+++ src/businesses/containers/Signin/Signin.js
@@ -1,119 +1,123 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux'
-import { signIn } from '../../../store/actions/actions'
-import { Redirect } from 'react-router-dom'
-import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
-import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { signIn } from "../../../store/actions/actions";
+import { Redirect } from "react-router-dom";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Avatar from "@material-ui/core/Avatar";
+import Typography from "@material-ui/core/Typography";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
 import { Link } from "react-router-dom";
-import Header from '../../components/Header/Header';
+import Header from "../../components/Header/Header";
 
-import './SignIn.css'
+import "./SignIn.css";
 
-class Signin extends Component {
+class SignIn extends Component {
   state = {
-    email: '',
-    password: ''
-  }
+    email: "",
+    password: "",
+  };
   handleChange = (e) => {
     this.setState({
-      [e.target.id]: e.target.value
-    })
-  }
+      [e.target.id]: e.target.value,
+    });
+  };
 
   handleSubmit = (e) => {
     e.preventDefault();
-    this.props.signIn(this.state)
-  }
+    this.props.signIn(this.state);
+  };
   render() {
+    const { authError, auth, classes } = this.props;
+    if (auth.uid) return <Redirect to="/Feed" />;
 
-    const { authError, auth, classes } = this.props
-    if (auth.uid) return <Redirect to='/Feed' />
-
-    return (<>
-      <Header />
-      <div className="container">
-
-        <form onSubmit={this.handleSubmit} className="white">
-
-          <div style={{display: 'inline-block', marginTop: '2rem'}}>
-          <Avatar style={{backgroundColor: '#dd004f', fontSize: '2rem'}}>
-            <LockOutlinedIcon />
-          </Avatar>
-          </div>
-        
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-
-          <div className="SignIn">
-          <TextField 
-            className="SignIn"
-            style={{marginTop: '2rem'}}
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            notched
-            name="email"
-            autoComplete="email"
-            onChange={this.handleChange}
-          />
-          <br></br>
-          <TextField 
-            // style={{width: '50%'}}
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="password"
-            label="Password"
-            type="password"
-            name="password"
-            onChange={this.handleChange}
-          />
-          <div className="submit">
-            <Button
-              style={{marginTop: '1rem', marginBottom: '2rem', color: 'white', backgroundColor: '#0378d8'}}
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="primary"
-            >
-              Sign In
-            </Button>
-            <p>
-              <Link style={{textDecoration: 'none', color: '#0378d8'}} to="/SignUp">Don't have an account? Sign Up</Link>
-            </p>
-            <div>
-              { authError ? <p>{authError}</p> : null}
+    return (
+      <>
+        <Header />
+        <div className="container">
+          <form onSubmit={this.handleSubmit} className="white">
+            <div style={{ display: "inline-block", marginTop: "2rem" }}>
+              <Avatar style={{ backgroundColor: "#dd004f", fontSize: "2rem" }}>
+                <LockOutlinedIcon />
+              </Avatar>
             </div>
-          </div>
-          </div>
 
-        </form>
+            <Typography component="h1" variant="h5">
+              Sign in
+            </Typography>
 
-      </div>
+            <div className="SignIn">
+              <TextField
+                className="SignIn"
+                style={{ marginTop: "2rem" }}
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                notched
+                name="email"
+                autoComplete="email"
+                onChange={this.handleChange}
+              />
+              <br></br>
+              <TextField
+                // style={{width: '50%'}}
+                variant="outlined"
+                margin="normal"
+                required
+                fullWidth
+                id="password"
+                label="Password"
+                type="password"
+                name="password"
+                onChange={this.handleChange}
+              />
+              <div className="submit">
+                <Button
+                  style={{
+                    marginTop: "1rem",
+                    marginBottom: "2rem",
+                    color: "white",
+                    backgroundColor: "#0378d8",
+                  }}
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                >
+                  Sign In
+                </Button>
+                <p>
+                  <Link
+                    style={{ textDecoration: "none", color: "#0378d8" }}
+                    to="/SignUp"
+                  >
+                    Don't have an account? Sign Up
+                  </Link>
+                </p>
+                <div>{authError ? <p>{authError}</p> : null}</div>
+              </div>
+            </div>
+          </form>
+        </div>
       </>
-    )
+    );
   }
 }
 
 const mapStateToProps = (state) => {
   return {
     authError: state.auth.authError,
-    auth: state.firebase.auth
-  }
-}
+    auth: state.firebase.auth,
+  };
+};
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    signIn: (creds) => dispatch(signIn(creds))
-  }
-}
+    signIn: (creds) => dispatch(signIn(creds)),
+  };
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Signin)
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
